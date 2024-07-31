ab05b05779ef6c009d94f9afeadbdeea6d2e5faa
Zwei <zwei.elen@outlook.com>
2024-07-31 21:08:17 +0800

    ci: add build info

--- cmd/pc/main.go
+++ cmd/pc/main.go
@@ -3,19 +3,35 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"runtime"
 
 	"github.com/nilluan304/pc/pc"
 )
 
-var config = flag.String("config", "config.json", "config file")
-
-// TODO
-// handle log file
-// control display by PowerShell script
+var (
+	version   = "v0.0.0"
+	buildTime = "2006-01-02 15:04:05"
+	commit    = ""
+	ref       = ""
+)
 
 func main() {
+	fmt.Fprintf(os.Stdout, `Welcome to PC! 
+Env Info:
+    Git Commit: %s
+    PC Version: %s
+    Build Time: %s
+    Go Version: %s
+Ref: %s
+`, commit, version, buildTime, runtime.Version(), ref)
+
+	var (
+		config = flag.String("config", "config.json", "config file")
+	)
+
 	flag.Parse()
 
 	file, err := os.ReadFile(*config)
